1ca8a1bd61b8d44f8938da681041ebf66c1302f1
cllafleur@gmail.com <cllafleur@gmail.com>
2014-10-17 18:36:34 +0200

    Add rules loading

---
+++ SchedulerSimulator/SchedulerSimulator/Schedule.Ruler/ReplanLowPriorityJobRulesBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace SchedulerSimulator.Schedule.Ruler {
+	class ReplanLowPriorityJobRulesBuilder {
+
+		public static Rule[] GetRules() {
+			DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Rule[]));
+			using (FileStream stream = new FileStream("DataRules.json", FileMode.Open, FileAccess.Read)) {
+				object o = serializer.ReadObject(stream);
+				return (Rule[])o;
+			}
+		}
+	}
+}
